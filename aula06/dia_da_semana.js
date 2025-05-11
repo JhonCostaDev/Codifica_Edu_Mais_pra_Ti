25c06f0f0ad93ee72851a1f53e1611dd595ca065
Jonathan Costa <jhoncosta2@gmail.com>
2025-05-10 23:31:07 -0300

    complete: aula06 and lista2_09.js

--- aula06/dia_da_semana.js
+++ aula06/dia_da_semana.js
@@ -3,5 +3,18 @@
 //esse dia deve ser um texto
 // se for sabado ou domingo imprimir: Final de semana
 //senão: Dia útil
+const input = require("prompt-sync")();
+const weekDays = ["domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado"];
 
+let userDay = input("What's day is today?  ").toLowerCase();
+
+if (weekDays.includes(userDay)){
+    if(userDay == weekDays[0] || userDay == weekDays.slice(-1)[0]){
+        console.log('Weekend');
+    } else {
+        console.log('weekday');
+    }
+} else {
+    console.log('Invalid entry!!!')
+}
 
