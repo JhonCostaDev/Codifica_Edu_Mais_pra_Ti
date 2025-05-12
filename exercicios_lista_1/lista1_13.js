0258289e41f4d13bda40e6a18a61a50a79518cff
Jonathan Costa <jhoncosta2@gmail.com>
2025-05-12 11:45:43 -0300

    add: data.csv, concluded: lista2_19 - 20.js

--- exercicios_lista_1/lista1_13.js
+++ exercicios_lista_1/lista1_13.js
@@ -4,17 +4,36 @@
 const input = require('prompt-sync')();
 
 let numbers = [];
-while (true){
-    userInput = Number(input('Please enter a decimal number. Type 0 to exit:  '));
-    if (userInput == 0) {
-        break;
-    }   
-    numbers.push(userInput);
+
+function isDecimal(value){
+    return /^-?\d+\.\d+$/.test(value.toString());
+    
 }
 
-let sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue);
+while (true){
+    try{
+        userInput = Number(input('Please enter a decimal number. Type 0 to exit:  '));
+        if (userInput == 0) {
+            break;
+        }   
+        let decimal = isDecimal(userInput);
+        if(!decimal) {
+            throw new Error("It is not a Decimal number!!!");
+            
+        } else {
 
-let average = sum / numbers.length;
+            numbers.push(userInput);
+        }
+    } catch (erro) {
+        console.log(erro.message);
+    }
+}
+if(numbers.length > 0){
+    let sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue);
+    
+    let average = sum / numbers.length;
+    console.log(`The average of the numbers typed is: ${average}`)
 
+}
 
-console.log(`The average of the numbers typed is: ${average}`)
+//console.log(isDecimal(2.190))
