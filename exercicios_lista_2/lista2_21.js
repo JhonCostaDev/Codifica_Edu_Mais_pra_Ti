42ea42607045e930671eed04e61a091495028c71
Jonathan Costa <111503993+JhonCostaDev@users.noreply.github.com>
2025-05-17 19:22:40 +0000

    inicio_aula12_min_35

--- exercicios_lista_2/lista2_21.js
+++ exercicios_lista_2/lista2_21.js
@@ -1,4 +1,25 @@
 // 21. Faça uma função que recebe, por parâmetro, a altura (alt) e o sexo de uma pessoa e
 // retorna o seu peso ideal. Para homens, calcular o peso ideal usando a fórmula: peso ideal
 // = 72.7 x alt - 58 e, para mulheres, peso ideal = 62.1 x alt - 44.7.
+const input = require('prompt-sync')();
+
+function idealWeight(height, gender) {
+    
+    if (gender == 'm') {
+        return 72.7 * height - 58;
+    } else {
+        return  62.1 * height - 44.7;
+    }
+}
+
+let userHeight = Number(input('Digite sua altura:   '));
+let userGender = input("Digite < M > - Masculino ou < F > - Feminino:   ").toLowerCase();
+
+
+let result = idealWeight(userHeight, userGender);
+
+console.log(`Seu peso ideal deve ser ${result.toFixed(2)}`);
+
+
+
 
