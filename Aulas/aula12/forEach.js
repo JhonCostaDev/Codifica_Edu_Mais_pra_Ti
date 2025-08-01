96aa74f67937dc63a0e96e891f0add9dcc19e7fa
Jonathan Costa <jhoncosta2@gmail.com>
2025-08-01 15:24:20 -0300

    finish exercicio proposto base

--- Aulas/aula12/forEach.js
+++ Aulas/aula12/forEach.js
@@ -6,3 +6,21 @@ a.forEach((num) => {
 let fruits = ['apple', 'strawberry', 'grape', 'orange', 'banana']
 
 fruits.forEach((fruit) => {console.log(fruit)})
+
+// =======================
+//edited on August 1, 2025
+const frutas = ["maçã", "banana", "laranja"];
+
+frutas.forEach((item, index) => {
+    console.log(`${index}: ${item}`);
+})
+
+fruits = [
+    "apple", "banana", "orange", "grape", "watermelon",
+    "pineapple", "strawberry", "blueberry", "mango", "kiwi",
+    "pear", "peach", "plum", "cherry", "coconut",
+    "papaya", "pomegranate", "lime", "lemon", "raspberry"
+]
+fruits.forEach((item, index) => {
+    console.log(`${index}: ${item}`);
+})
--- Aulas/aula32/exercicioProposto/app.js
+++ Aulas/aula32/exercicioProposto/app.js
@@ -8,3 +8,61 @@ const cleanList = document.getElementById('cleanList');
 
 let list = [];
 
+window.addEventListener('DOMContentLoaded', () => {
+    const data = localStorage.getItem('listaCompras');
+    list = JSON.parse(data);
+    showOutput();
+});
+
+
+listForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const newItem = inItem.value.trim();
+    
+
+    if(!newItem) return;
+
+    list.push(newItem);
+    saveData();
+    showOutput();
+    inItem.value = ''; // limpa o campo
+});
+
+function showOutput() {
+    outList.innerHTML = '';
+
+    list.forEach((item, index) => {
+        const li = document.createElement('li');
+        const span = document.createElement('span');
+        span.textContent = item;
+
+        const btnRemove = document.createElement('button');
+        btnRemove.textContent = 'x';
+        btnRemove.title = 'Remover Item';
+
+        btnRemove.addEventListener('click', () => {
+            removeItem(index); //FAZER AQUI
+        })
+        li.append(span, btnRemove);
+        outList.appendChild(li);
+    })
+}
+
+function removeItem(index) {
+    list.splice(index, 1);
+    saveData();
+    showOutput();
+}
+
+function saveData () {
+    localStorage.setItem('listaCompras', JSON.stringify(list));
+}
+
+cleanList.addEventListener('click', () => {
+    if(confirm('Deseja Limpar a Lista?')) {
+        list = [];
+        saveData();
+        showOutput();
+        
+    }
+})
