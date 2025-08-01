99195a16ff7bc5dc061706614c6c11ef3ad1d8da
Jonathan Costa <jhoncosta2@gmail.com>
2025-08-01 11:14:25 -0300

    aula32/exercicio_proposto inicio

---
+++ Aulas/aula32/exercicioProposto/app.js
@@ -0,0 +1,10 @@
+const listForm = document.getElementById('listForm');
+
+const inItem = document.getElementById('inItem');
+
+const outList  = document.getElementById('outList');
+
+const cleanList = document.getElementById('cleanList');
+
+let list = [];
+
