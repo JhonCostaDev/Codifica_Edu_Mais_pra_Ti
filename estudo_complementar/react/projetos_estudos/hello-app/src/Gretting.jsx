540f2e57424a1fad86091e0c2ee6302f12f03e78
Jonathan Costa <jhoncosta2@gmail.com>
2025-09-22 09:56:43 -0300

    add gretting.jsx

---
+++ estudo_complementar/react/projetos_estudos/hello-app/src/Gretting.jsx
@@ -0,0 +1,10 @@
+export default function Greating (props){
+    return (
+        <>
+            <h1>Hello, {props.name}</h1>
+            <p>Welcome to react learning. I'm so proud with you. {props.date}</p>
+        
+        </>
+    ) 
+}
+
--- estudo_complementar/react/projetos_estudos/hello-app/src/index.js
+++ estudo_complementar/react/projetos_estudos/hello-app/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Hello from './Hello';
+import Greating from './Gretting';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Hello />
-    <App />
+    <Greating name='Jhon' date='Monday, September 22, 2025'/>
+    {/* <App /> */}
   </React.StrictMode>
 );
 
