081b5649ba717889d0c3c633a794ba94cba8c4d7
Jonathan Costa <jhoncosta2@gmail.com>
2025-09-22 15:06:34 -0300

    add deitel java8 ex3.17 - Health Profile - inicio

---
+++ estudo_complementar/java/deitel/healthProfile/HealthProfile.java
@@ -0,0 +1,31 @@
+//package estudo_complementar.java.deitel.healthProfile;
+
+public class HealthProfile {
+    String name;
+    String surName;
+    char gender;
+    int birthDay;
+    int birthMonth;
+    int birthYear;
+    double height;
+    double weight;
+
+    //Constructor
+    public HealthProfile(String name, String surName, char gender, int birthDay, int birthMonth, int birthYear, double height, double weight) {
+        this.name = name;
+        this.surName = surName;
+        this.gender = gender;
+        this.birthDay = birthDay;
+        this.birthMonth = birthMonth;
+        this.birthYear = birthYear;
+    }
+    //Age
+
+    //Body Mass Index BMI
+
+    // Heart Rates
+
+    //Getters
+
+    //Setters
+}
---
+++ estudo_complementar/java/deitel/healthProfile/enunciado.md
@@ -0,0 +1,21 @@
+# Health Profile
+exercício 3.17
+
+Projete uma classe chamada **HealthProfile** que possua os seguintes atributos:
+    * nome
+    * sobrenome
+    * sexo
+    * data de nascimento (dia, mes, ano) separados
+    * altura (metros)
+    * peso (Kg)
+
+Sua classe deve ter um construtor que receba esses dados.
+
+Para cada atributo forne;a os métodos get e set.
+
+A classe  deve ter  métodos que:
+    * Calcule e retorne a idade do usuário em anos,
+    * Intervalo de frequência cardíaca Máxima
+    * Frequência cardíaca Alvo
+    * IMC - Índice de massa corporal
+
