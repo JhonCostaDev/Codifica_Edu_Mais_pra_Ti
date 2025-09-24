3fafd1e185b36f23ca681f5c57952e83228b3f63
Jonathan Costa <jhoncosta2@gmail.com>
2025-09-24 15:19:22 -0300

    inicio exercicio 3 - lista de tarefas 7 java poo

---
+++ Exercicios/lista_tarefas7_java_POO/03_Funcionario/Funcionario.java
@@ -0,0 +1,18 @@
+// package Exercicios.lista_tarefas7_java_POO.03_Funcionario;
+
+import java.math.BigDecimal;
+
+public class Funcionario {
+   protected String nome;
+   protected BigDecimal salario; 
+
+   //getters
+
+   public String getNome(){
+    return nome;
+   }
+
+   public BigDecimal getSalario() {
+    return salario;
+   }
+}
