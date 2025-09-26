8b2de221e6d4d3a2694db4e7a67a089e6e0c3544
Jonathan Costa <jhoncosta2@gmail.com>
2025-09-26 16:13:21 -0300

    add: Java class Student - deitel java 8 ex 4.4

---
+++ estudo_complementar/java/deitel/student/Main.java
@@ -0,0 +1,10 @@
+package estudo_complementar.java.deitel.student;
+
+public class Main {
+    public static void main(String[] args) {
+        Student josy = new Student("Josyllane",  85);
+
+        System.out.println("Nota: " + josy.getLetterGrade());
+
+    }
+}
