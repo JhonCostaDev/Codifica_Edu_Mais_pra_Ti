0739fcd7e801a0af07482b6a272b691f9fabda4b
Jonathan Costa <jhoncosta2@gmail.com>
2025-09-27 23:55:32 -0300

    add: pag-cadastro-visitantesANM

---
+++ estudo_complementar/html/anm/visitantes/vanilha/script.js
@@ -0,0 +1,49 @@
+const visitantes = [
+    {
+        name: 'luis perera',
+        cpf: 45645645687,
+        phone: 8598789878,
+        servidor: "saulo"
+    },
+    {
+        name: 'Maria Luiza',
+        cpf: 45645645687,
+        phone: 8598789878,
+        servidor: "Helano"
+    },
+    {
+        name: 'Chucha Saboia',
+        cpf: 6547889221,
+        phone: 6547889221,
+        servidor: "Helano"
+    }
+];
+const list = document.querySelector("#list");
+list.innerHTML = `
+    <h2>Visitantes</h2>
+    <p><span>Nome</span> - <span>CPF</span> - <span>Setor/Servidor</span><span> Saida</span></p>
+    `
+visitantes.forEach(visitante => {
+    const item = document.createElement("li");
+    item.innerHTML = `
+    <span>${visitante.name}  ${visitante.cpf} ${visitante.servidor}</span>
+    <button onclick="removerItem(this)">Saida</button>
+    `
+    list.appendChild(item)
+});
+
+function removerItem(btn) {
+    const item = btn.parentElement;
+    item.remove();
+}
+
+function saveData(obj) {
+    visitantes.push(obj);
+}
+
+document.querySelector("#cadastro").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = document.querySelector("#name").value;
+    
+})
+
--- estudo_complementar/javaScript/lógicaDeProgramaçãoEAlgoritmos_EdécioFernando/LanhouseJS/script.js
+++ estudo_complementar/javaScript/lógicaDeProgramaçãoEAlgoritmos_EdécioFernando/LanhouseJS/script.js
@@ -1,11 +1,10 @@
-document.querySelector("#form").addEventListener("submit", function(e) {
+document.querySelector("#form").addEventListener("submit", (e) => {
     e.preventDefault();
     const hourPrice = Number(document.querySelector("#hourPrice").value);
     const timeSpent = document.querySelector("#timeSpent").value;
     const output = document.querySelector("#output");
     const result = timeSpent * hourPrice;
     //alert("Foi");
-    output.innerHTML = `Total a pagar: R$${result.toFixed(2)}`;
-
-
+    output.innerHTML = `<p>Total a pagar:  R$${result.toFixed(2)}</p>`;
+    output.appendChild(p)
 })
