ea4b276ae937c1a3a2bd940e936106bd0142c4ae
Jonathan Costa <jhoncosta2@gmail.com>
2025-10-01 12:09:06 -0300

    add: estudo complementar logica-Edecio - cap3 - situacao do aluno

--- estudo_complementar/logicaEAlgoritmos_EdecioFernandes/cap3/situacaoAluno/script.js
+++ estudo_complementar/logicaEAlgoritmos_EdecioFernandes/cap3/situacaoAluno/script.js
@@ -1,4 +1,27 @@
-document.querySelector("#calcAverage").addEventListener("submit", (e) => {
-    const name = document.querySelector("#name").value;
+function calcAverage(gradeone, gradetwo) {
+    const sum = gradeone + gradetwo;
+    const average = sum / 2;
+    return average;
+}
+
+function verifySituation(student, average) {
+    if(average < 7) return `Com média ${average} o aluno ${student} foi REPROVADO`;
     
+    return `Com média ${average} o aluno ${student} foi APROVADO`;
+}
+
+document.querySelector("#calcAverage").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = document.querySelector("#name").value;
+
+    const firstGrade = Number(document.querySelector("#firstGrade").value);
+
+    const secondGrade = Number(document.querySelector("#secondGrade").value);
+
+    const outAverage = document.querySelector("#outAverage");
+    const outSituation = document.querySelector("#outSituation");
+
+    const average = calcAverage(firstGrade, secondGrade);
+
+    outSituation.innerHTML = verifySituation(name, average);
 })
