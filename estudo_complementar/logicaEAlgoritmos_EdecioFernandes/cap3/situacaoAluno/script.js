df3248677d2b0a58438907e282421dc04f4b0cc7
Jonathan Costa <jhoncosta2@gmail.com>
2025-10-15 21:32:44 -0300

    refactor estudo_complementar/javascript/logica_programacao/cap3/situacao_aluno

--- estudo_complementar/logicaEAlgoritmos_EdecioFernandes/cap3/situacaoAluno/script.js
+++ estudo_complementar/logicaEAlgoritmos_EdecioFernandes/cap3/situacaoAluno/script.js
@@ -1,27 +1,54 @@
-function calcAverage(gradeone, gradetwo) {
-    const sum = gradeone + gradetwo;
-    const average = sum / 2;
-    return average;
+function calcAverage(arr) {
+    if (arr.length === 0) return 0;
+    const sum = arr.reduce((acc, curr) => acc + curr, 0);
+    return sum / arr.length;  
 }
 
-function verifySituation(student, average) {
-    if(average < 7) return `Com média ${average} o aluno ${student} foi REPROVADO`;
+function verifySituation(student, average, subject) {
     
-    return `Com média ${average} o aluno ${student} foi APROVADO`;
+    
+    if(average < 7) return `Com média ${average} o aluno ${student} foi REPROVADO em ${subject}`;
+    
+    return `Com média ${average} o aluno ${student} foi APROVADO em ${subject}`;
 }
 
+function registerResult(name, subject, grades, average, situation) {
+    return {
+        aluno: name,
+        materia: {
+            nome: subject,
+            notas: grades,
+            media_final: average,
+            situacao: situation
+        }
+    }
+}
+
+const alunos = [];
 document.querySelector("#calcAverage").addEventListener("submit", (e) => {
+    const grades = [];
     e.preventDefault();
     const name = document.querySelector("#name").value;
 
+    const subject = document.querySelector("#subject");
+    const subjectValue = subject.value;
+
     const firstGrade = Number(document.querySelector("#firstGrade").value);
-
+    grades.push(firstGrade);
     const secondGrade = Number(document.querySelector("#secondGrade").value);
-
+    grades.push(secondGrade);
+    const thirdGrade = Number(document.querySelector("#thirdGrade").value);
+    grades.push(thirdGrade);
+    const fourthGrade = Number(document.querySelector("#fourthGrade").value);
+    grades.push(fourthGrade);
+    
+    const average = calcAverage(grades);
+    
     const outAverage = document.querySelector("#outAverage");
     const outSituation = document.querySelector("#outSituation");
-
-    const average = calcAverage(firstGrade, secondGrade);
-
-    outSituation.innerHTML = verifySituation(name, average);
-})
+    const situation = (average >= 7 ? "Aprovado": "Reprovado");
+    
+    alunos.push(registerResult(name, subjectValue, grades, average, situation));
+    outSituation.innerHTML = verifySituation(name, average, subjectValue);
+});
+console.log(alunos);
