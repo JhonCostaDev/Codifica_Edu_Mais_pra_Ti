858e970df8f304613039d805c75adc623efd931c
Jonathan Costa <jhoncosta2@gmail.com>
2025-10-20 16:22:36 -0300

    finish estudo_complementar/java/deital/chapter4 - credit limit

--- estudo_complementar/java/deitel/Chapter_4/4.18_creditLimitCalculator/src/Customer.java
+++ estudo_complementar/java/deitel/Chapter_4/4.18_creditLimitCalculator/src/Customer.java
@@ -1,22 +1,25 @@
 public class Customer {
     private int accountNumber; //Numero da conta
     private int accountStartMonthBalance; //Saldo inicial do mes
-    private int accountTotalDiscount; //Desconto total
-    private int amountCreditGrantedMonth; //Valor de credito concedido no mes
+    private int totalCreditsApplied; //Desconto total
+    private int totalChargesThisMonth; //Valor de credito concedido no mes
     private int creditLimitAvailable; //Limite de credito disponivel
-    private int newBalance;
     
-    public Customer(int accountNumber, int accountStartMonthBalance, int accountTotalDiscount, int amountCreditGrantedMonth, int creditLimitAvailable) {
+    
+    public Customer(int accountNumber, int accountStartMonthBalance, int totalCreditsApplied, int totalChargesThisMonth, int creditLimitAvailable) {
         this.accountNumber = accountNumber;
         this.accountStartMonthBalance = accountStartMonthBalance;
-        this.accountTotalDiscount = accountTotalDiscount;
-        this.amountCreditGrantedMonth = amountCreditGrantedMonth;
+        this.totalCreditsApplied = totalCreditsApplied;
+        this.totalChargesThisMonth  = totalChargesThisMonth;
         this.creditLimitAvailable = creditLimitAvailable;
-        this.newBalance = accountStartMonthBalance + amountCreditGrantedMonth - accountTotalDiscount;
+    }
+    
+    public int calculateNewBalance() {
+        return  accountStartMonthBalance + totalChargesThisMonth - totalCreditsApplied;
     }
     
     public String isCreditLimitAvailable() {
-        if (newBalance > creditLimitAvailable) {
+        if (calculateNewBalance() > creditLimitAvailable) {
             return "Limite de credito excedido";
         } else {
             return "Limite de credito disponivel";
--- estudo_complementar/java/deitel/Chapter_4/4.18_creditLimitCalculator/src/Main.java
+++ estudo_complementar/java/deitel/Chapter_4/4.18_creditLimitCalculator/src/Main.java
@@ -1,14 +1,42 @@
 public class Main {
     public static void main(String[] args) {
+        
+        Customer cliente1 = new Customer(
+            1001,  // Número da conta
+            500,   // Saldo inicial do mês
+            200,   // Total de créditos aplicados (pagamentos)
+            300,   // Total de compras no mês
+            1000   // Limite de crédito
+        );
+        
+        Customer cliente2 = new Customer(
+            1002,
+            400,
+            100,
+            300,
+            600
+        );
+        
+        Customer cliente3 = new Customer(
+            1003,
+            800,
+            100,
+            500,
+            1100
+        );
+        
+        System.out.println("cliente1: " + cliente1.calculateNewBalance() + cliente1.isCreditLimitAvailable());
+        
+        System.out.println("cliente2: " + cliente2.calculateNewBalance() + cliente2.isCreditLimitAvailable());
+        
+        System.out.println("cliente3: " + cliente3.calculateNewBalance() + cliente3.isCreditLimitAvailable());
+        
+        
 
-        int registro = 1234;
-        String nomeCliente = "Francisco Messias Oliveira";
-        int saldoMes = 1000;
-        int gastoMensal = 800;
-        int creditoUtilizado = 500;
-        int limiteCredito = 1000;
 
-        Customer fcoMessias = new Customer(registro, saldoMes, gastoMensal, creditoUtilizado, limiteCredito);
-        System.out.println(fcoMessias.isCreditLimitAvailable());
+        
+
+        
     }
+
 }
